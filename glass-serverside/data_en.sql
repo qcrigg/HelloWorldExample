7264afa71b6ded95b6883266d8af21aaa4f5a851
qcrigg <qcrigg@users.noreply.github.com>
2014-08-24 13:46:19 +0300

    Update data_en.sql

--- glass-serverside/data_en.sql
+++ glass-serverside/data_en.sql
@@ -16,6 +16,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+create database qcri;
+use qcri;
+
 --
 -- Database: `qcri`
 --
